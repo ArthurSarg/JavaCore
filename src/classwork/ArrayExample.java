5894f1a8140a97e071a60e3413883d5f9c8928f9
ArthurSarg <sargsyanarthur79@gmail.com>
2024-03-16 12:50:25 +0400

    First project

--- src/classwork/ArrayExample.java
+++ src/classwork/ArrayExample.java
@@ -1,7 +1,14 @@
 package classwork;
 
+import java.util.Random;
+
 public class ArrayExample {
     public static void main(String[] args) {
         int[] numbers = new int[5];
+        Random random = new Random();
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = 2 + random.nextInt(4);
+            System.out.println(numbers[i]);
+        }
     }
 }
